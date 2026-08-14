5bd23709a5cebcb4de5667e5e76d450ba182fdf7
Taron <taron38@yandex.ru>
2026-08-14 20:01:46 +0300

    change font to Noto Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Noto_Sans } from 'next/font/google'
 import { UploadProvider } from '@/lib/UploadContext'
 import AppFooter from '@/components/AppFooter'
 import PostEditorWrapper from '@/components/PostEditorWrapper'
+
+const notoSans = Noto_Sans({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-noto-sans',
+})
 
 export const metadata: Metadata = {
   title: 'VAIR',
@@ -15,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ru">
-      <body className="bg-black">
+    <html lang="ru" className={notoSans.variable}>
+      <body className={`${notoSans.className} bg-black`}>
         <UploadProvider>
           <div className="flex justify-center min-h-screen">
             <div className="w-full max-w-[460px] bg-zinc-900 flex flex-col min-h-screen">
